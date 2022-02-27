8f7a9610bae564142e002c91b467bbf6753e1b4d
Peter Dinis <peterdinis0@pobox.sk>
2022-02-27 12:41:00 +0100

    Update logic for login/register

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,18 @@
 import { AuthService } from './auth.service';
-import { Controller, Post } from "@nestjs/common";
+import { Controller, Post, Body } from "@nestjs/common";
+import { AuthDto } from './auth.dto';
 
 @Controller()
 export class AuthController {
     constructor(private authService: AuthService) {}
 
     @Post("signup")
-    signup() {}
+    signup(@Body() dto: AuthDto) {
+        return this.authService.signup(dto);
+    }
 
     @Post("signin")
-    signin() {}
+    signin(@Body() dto: AuthDto) {
+        return this.authService.signin(dto);
+    }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,60 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import {PrismaService} from "../prisma/prisma.service";
+import { AuthDto } from "./auth.dto";
+import * as argon from "argon2";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 
 @Injectable()
 export class AuthService {
     constructor(private prisma: PrismaService) {}
     
-    signup() {}
-    signin() {}
+    async signup(dto: AuthDto) {
+        try {
+            // generate password
+        const hash = await argon.hash(dto.password);
+        // save user in db
+
+        const user = await this.prisma.user.create({
+            data: {
+                email: dto.email,
+                hash,
+            },
+
+            // this we want return
+            select: {
+                id: true,
+                email: true,
+                createdAt: true
+            }
+        });
+        return user;
+        } catch(error) {
+            if(error instanceof PrismaClientKnownRequestError) {
+                if(error.code === "P2002") {
+                    throw new ForbiddenException("Credentials taken");
+                } // you create record with unique field
+            }
+
+            throw error;
+        }
+    }
+
+
+    async signin(dto: AuthDto) {
+        // find user by email
+        const user = await this.prisma.user.findUnique({
+            where: {
+                email: dto.email
+            }
+        }) ; 
+        // if user not exists error
+        if(!user) throw new ForbiddenException("Credentails incorect");
+        // compare passwrod
+        const passwordMatches = await argon.verify(user.hash, dto.password);
+        if(!passwordMatches) throw new ForbiddenException("Credentails incorect");
+        // if password is not the same throw error
+
+        delete user.hash;
+        return user;
+    }
 }
