3737cc8456cc586d65a3207306dc9550d25b8de8
Peter Dinis <peterdinis0@pobox.sk>
2022-03-01 18:59:18 +0100

    Updating auth

---
+++ prisma/migrations/20220301174317_update_user/migration.sql
@@ -0,0 +1,26 @@
+/*
+  Warnings:
+
+  - You are about to drop the `users` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "users";
+
+-- CreateTable
+CREATE TABLE "User" (
+    "bio" TEXT,
+    "email" TEXT NOT NULL,
+    "id" SERIAL NOT NULL,
+    "image" TEXT,
+    "password" TEXT,
+    "username" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -19,6 +19,19 @@ export class AuthService {
     private prisma: PrismaService
   ) {}
 
+  public generateJWT(user) {
+    let today = new Date();
+    let exp = new Date(today);
+    exp.setDate(today.getDate() + 60);
+
+    return jwt.sign({
+      id: user.id,
+      username: user.username,
+      email: user.email,
+      exp: exp.getTime() / 1000,
+    }, "SOMELATER");
+  };
+
   async findAll(): Promise<any[]> {
     return await this.prisma.user.findMany({ select });
   }
@@ -34,7 +47,7 @@ export class AuthService {
       throw new HttpException({errors}, 401);
     }
 
-    const authenticated = await argon2.verify(_user., payload.password);
+    const authenticated = await argon2.verify(_user.password, payload.password);
 
     if (!authenticated) {
       throw new HttpException({errors}, 401);
@@ -45,5 +58,51 @@ export class AuthService {
     return {
       user: {token, ...user}
     };
+  }
+
+  async create(dto: CreateUserDto): Promise<UserRO> {
+    const {username, email, password} = dto;
+
+    // check uniqueness of username/email
+    const userNotUnique = await this.prisma.user.findUnique({
+      where: {email}
+    });
+
+    if (userNotUnique) {
+      const errors = {username: 'Username and email must be unique.'};
+      throw new HttpException({message: 'Input data validation failed', errors}, HttpStatus.BAD_REQUEST);
+    }
+
+    const hashedPassword = await argon2.hash(password);
+
+    const data = {
+      username,
+      email,
+      password: hashedPassword,
+    };
+    const user = await this.prisma.user.create({ data, select });
+
+    return {user};
+  }
+
+  async update(id: number, data: UpdateUserDto): Promise<any> {
+    const where = { id };
+    const user = await this.prisma.user.update({ where, data, select });
+
+    return {user};
+  }
+
+  async delete(email: string): Promise<any> {
+    return await this.prisma.user.delete({ where: { email }, select });
+  }
+
+  async findById(id: number): Promise<any>{
+    const user = await this.prisma.user.findFirst({ where: { id }, select: {id: true, ...select} });
+    return { user };
+  }
+
+  async findByEmail(email: string): Promise<any>{
+    const user = await this.prisma.user.findFirst({ where: { email }, select });
+    return { user };
   }
 }
