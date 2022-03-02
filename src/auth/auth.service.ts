e99c0b47307ab98ae519c0c73aa396ef2f379100
Peter Dinis <peterdinis0@pobox.sk>
2022-03-02 19:50:04 +0100

    Fix user issue

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,90 +1,98 @@
-import { Injectable } from "@nestjs/common";
-import {PrismaService} from "../prisma/prisma.service";
-import { CreateUserDto, LoginUserDto, UpdateUserDto } from "./dto"
-import * as argon2 from 'argon2';
-import * as jwt from "jsonwebtoken";
-import { UserData, UserRO } from "./user.interface";
-import { HttpException } from '@nestjs/common/exceptions/http.exception';
-import { HttpStatus } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
-const select = {
-  email: true,
-  username: true,
-  bio: true,
-  image: true
-};
 
 @Injectable()
 export class AuthService {
   constructor(
-    private prisma: PrismaService
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
   ) {}
 
-  public generateJWT(user) {
-    let today = new Date();
-    let exp = new Date(today);
-    exp.setDate(today.getDate() + 60);
+  async signup(dto: AuthDto) {
+    // generate the password hash
+    const hash = await argon.hash(dto.password);
+    // save the new user in the db
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
 
-    return jwt.sign({
-      id: user.id,
-      username: user.username,
-      email: user.email,
-      exp: exp.getTime() / 1000,
-    }, "SOMELATER");
-  };
-
-  async findAll(): Promise<any[]> {
-    return await this.prisma.user.findMany({ select });
+      return this.signToken(user.id, user.email);
+    } catch (error) {
+      if (
+        error instanceof
+        PrismaClientKnownRequestError
+      ) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException(
+            'Credentials taken',
+          );
+        }
+      }
+      throw error;
+    }
   }
 
-  async login(payload: LoginUserDto){
-    const _user = await this.prisma.user.findUnique({
-      where: {email: payload.email}
-    });
+  async signin(dto: AuthDto) {
+    // find the user by email
+    const user =
+      await this.prisma.user.findUnique({
+        where: {
+          email: dto.email,
+        },
+      });
+    // if user does not exist throw exception
+    if (!user)
+      throw new ForbiddenException(
+        'Credentials incorrect',
+      );
 
-    const errors = { User: 'email or password wrong' };
+    // compare password
+    const pwMatches = await argon.verify(
+      user.hash,
+      dto.password,
+    );
+    // if password incorrect throw exception
+    if (!pwMatches)
+      throw new ForbiddenException(
+        'Credentials incorrect',
+      );
+    return this.signToken(user.id, user.email);
+  }
 
-    if (!_user) {
-      throw new HttpException({errors}, 401);
-    }
-
-    const authenticated = await argon2.verify(_user.password, payload.password);
-
-    if (!authenticated) {
-      throw new HttpException({errors}, 401);
-    }
-
-    const token = await this.generateJWT(_user);
-    const {password, ...user} = _user;
-    return {
-      user: {token, ...user}
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payload = {
+      sub: userId,
+      email,
     };
-  }
+    const secret = this.config.get('JWT_SECRET');
 
-  async create(data: CreateUserDto){
-    return this.prisma.user.create({
-      data
-    })
-  }
+    const token = await this.jwt.signAsync(
+      payload,
+      {
+        expiresIn: '15m',
+        secret: secret,
+      },
+    );
 
-  async update(id: number, data: UpdateUserDto): Promise<any> {
-    const where = { id };
-    const user = await this.prisma.user.update({ where, data, select });
-
-    return {user};
-  }
-
-  async delete(email: string): Promise<any> {
-    return await this.prisma.user.delete({ where: { email }, select });
-  }
-
-  async findById(id: number): Promise<any>{
-    const user = await this.prisma.user.findFirst({ where: { id }, select: {id: true, ...select} });
-    return { user };
-  }
-
-  async findByEmail(email: string): Promise<any>{
-    const user = await this.prisma.user.findFirst({ where: { email }, select });
-    return { user };
+    return {
+      access_token: token,
+    };
   }
 }
